15cd92db276062c1686753b89aea2ea66beaf069
miurakazuma67 <miurakazuma67@gmail.com>
2021-02-16 15:24:08 +0900

    変更内容を保存

--- TaskPart1/ViewController.swift
+++ TaskPart1/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        totalLabel.text = "0"
     }
-
-
+    
+    @IBAction func add(_ sender: Any) {
+        let textFields: [UITextField] = [
+            textField1,
+            textField2,
+            textField3,
+            textField4,
+            textField5
+        ]
+        
+        var total = 0
+        
+        for textField in textFields {
+            total += Int(textField.text ?? "") ?? 0
+        }
+        totalLabel.text = String(total)
+    }
 }
 
